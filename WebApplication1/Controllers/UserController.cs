6c66b536e971d84bc6fe98e777fa20f85f7f714b
Vinh <Vinhhoangvh11a@gmail.com>
2023-02-11 18:54:04 +0700

    Add project files.

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Context;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+
+        [HttpPost("Authenticate")]
+
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users.
+                FirstOrDefaultAsync(x => x.Username == userObj.Username );
+            if(user == null)
+                return NotFound(new {Message = "User Not Found"});
+
+            return Ok(new {Message = "Login Success"});
+        }
+
+
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+           await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new { Message = "User Registered" });
+        }
+    }
+}
